01858457cfbf1f17ceea5ea8f579f812c5c3416c
Dore Guillaume <guillaumedore.76@hotmail.fr>
2021-01-22 10:02:40 +0100

    #24 : Add to the main view the navigation buttons panel. Proper rename folders and change target sdk to .NET 5 framework

---
+++ DistributionPlatform.Startup/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DistributionPlatform.Startup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool IsMainPage { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IsMainPage = false;
+        }
+
+        void Navigation_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = (sender as Button).Tag.ToString();
+            if (tag == "Home")
+            {
+
+            }
+            if (tag == "Import")
+            {
+
+            }
+        }
+
+        void OpenSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("button pressed");
+        }
+    }
+}

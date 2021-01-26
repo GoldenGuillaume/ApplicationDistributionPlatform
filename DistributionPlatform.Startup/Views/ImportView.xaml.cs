717fb9db1d2955a4d27f0b735d455a2965b15454
Dore Guillaume <guillaumedore.76@hotmail.fr>
2021-01-27 00:09:12 +0100

    #25: Implementation of the import view

---
+++ DistributionPlatform.Startup/Views/ImportView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DistributionPlatform.Startup.Views
+{
+    /// <summary>
+    /// Interaction logic for ImportView.xaml
+    /// </summary>
+    public partial class ImportView : UserControl
+    {
+        private string ThumbnailFileName { get; set; } = null;
+        private string SourcesDirectoryName { get; set; } = null;
+
+        public ImportView()
+        {
+            InitializeComponent();
+        }
+
+        void ThumbnailImport_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Image files(*.bmp;*.jpg;*.jpeg;*.png)|*.bmp;*.jpg;*.jpeg;*.png";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                this.ThumbnailFileName = fileDialog.FileName;
+                thumbnailFileName.Text = Path.GetFileName(fileDialog.FileName);
+            }
+        }
+
+        void SourcesImport_Click(object sender, RoutedEventArgs e)
+        {
+            using (var browserDialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult res = browserDialog.ShowDialog();
+
+                if (res == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(browserDialog.SelectedPath))
+                {
+                    this.SourcesDirectoryName = browserDialog.SelectedPath;
+                    sourcesDirectoryName.Text = new DirectoryInfo(browserDialog.SelectedPath).Name;
+                }
+            }
+        }
+
+        void SaveApp_Click(object sender, RoutedEventArgs e)
+        {
+            // get thumbnail and sources then save into database
+        }
+    }
+}

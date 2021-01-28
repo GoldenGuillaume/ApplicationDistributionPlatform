4f8be28dc517715cc8b1d85804cda65353d28e46
Guillaume Doré <guillaume.dore@key4events.com>
2021-01-28 14:16:14 +0100

    #39: configure dependency injection container in application at launch

--- DistributionPlatform.Startup/App.xaml.cs
+++ DistributionPlatform.Startup/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using DistributionPlatform.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
 
 namespace DistributionPlatform.Startup
 {
@@ -7,5 +13,37 @@ namespace DistributionPlatform.Startup
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    ConfigureServices(context.Configuration, services);
+                })
+                .Build();
+        }
+
+        private void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddDbContext<DistributionPlatformContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            _host.Services.GetRequiredService<MainWindow>().Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+            base.OnExit(e);
+        }
     }
 }
